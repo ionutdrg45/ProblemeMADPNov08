f18b7f9f27f01063646b8f7cccccd8088cc04cd3
Ionuț Drăgan <52635963+ionutdrg45@users.noreply.github.com>
2021-11-08 09:06:25 +0200

    Add files via upload

---
+++ TurnurileDinHanoi/TurnurileDinHanoi/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurnurileDinHanoi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            Console.Write("Numar discuri: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Solutia pentru " + n + " discuri:");
+            new Solver().Hanoi(n, 'a', 'b', 'c');
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TurnurileDinHanoi/TurnurileDinHanoi/Solver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurnurileDinHanoi
+{
+    class Solver
+    {
+        public void Hanoi(int n, char a, char b, char c)
+        {
+            if (n == 1) Console.WriteLine(a + " -> " + b);
+            else
+            {
+                Hanoi(n - 1, a, c, b);
+                Console.WriteLine(a + " -> " + b);
+                Hanoi(n - 1, c, b, a);
+            }
+        }
+    }
+}
