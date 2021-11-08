008bb4a28a0341b658a5a4c1f3437d2d136dd808
Ionuț Drăgan <52635963+ionutdrg45@users.noreply.github.com>
2021-11-08 18:36:36 +0200

    Add files via upload

---
+++ QuickSort/QuickSort/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int>() { 2, 4, 3, 5, 9, 11, 1, 20, 7, 6 };
+
+            new Solver().QuickSort(list, 0, list.Count - 1);
+
+            for(int i = 0; i < list.Count; i++)
+            {
+                Console.Write(list[i] + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ QuickSort/QuickSort/Solver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    class Solver
+    {
+        public void QuickSort(List<int> list, int p, int u)
+        {
+            int i = p, j = u;
+            int m;
+            int pivot = list[(p + u) / 2];
+            while(i <= j)
+            {
+                while (list[i] < pivot) i++;
+                while (list[j] > pivot) j--;
+                if(i <= j)
+                {
+                    m = list[i];
+                    list[i] = list[j];
+                    list[j] = m;
+                    i++;
+                    j--;
+                }
+            }
+            if (p < j) QuickSort(list, p, j);
+            if (i < u) QuickSort(list, i, u);
+        }
+    }
+}
